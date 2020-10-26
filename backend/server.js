89f2bad4a1b67bcd9882d9c8065482d1c4967d8e
marcospoto <marcospoto@hotmail.com>
2020-10-26 12:41:06 -0400

    node JS workshop 2

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,9 @@
 // import the needed node_modules.
 const express = require("express");
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const { top50 } = require("./data/data");
+const { restart } = require("nodemon");
 
 express()
   // Below are methods that are included in express(). We chain them for convenience.
@@ -18,7 +21,83 @@ express()
   // Nothing to modify above this line
   // ---------------------------------
   // add new endpoints here 👇
+  .get("/top50", (req, res) => {
+    res.status(200).json({
+      status: 200,
+      data: top50,
+    });
+  })
 
+  .get("/top50/song/:rank", (req, res) => {
+    const rank = req.params.rank;
+    const rankNumber = Number(rank);
+    const data = top50.find((song) => {
+      return song.rank === rankNumber;
+    });
+    if (!data) {
+      res.status(400).json({
+        status: 400,
+        message: "Song not found",
+      });
+    } else {
+      res.status(200).json({
+        status: 200,
+        data,
+      });
+    }
+  })
+
+  .get("/top50/artist/:artistName", (req, res) => {
+    const artistName = req.params.artistName;
+    const data = top50.find(
+      (song) => song.artist.toLowerCase() === artistName.toLowerCase()
+    );
+    if (data.length === 0) {
+      res.status(400).json({
+        status: 400,
+        message: "Artist not found",
+      });
+    } else {
+      res.status(200).json({
+        stauts: 200,
+        data,
+      });
+    }
+  })
+
+  .get("/top50/popular-artist", (req, res) => {
+    const artistArr = top50.map((song) => song.artist);
+    const data = artistArr
+      .sort(
+        (a, b) =>
+          artistArr.filter((v) => v === a).length -
+          artistArr.filter((v) => v === b).length
+      )
+      .pop();
+    res.status(200).json({
+      stauts: 200,
+      data,
+    });
+  })
+
+  .get("/top50/artist", (req, res) => {
+    const artistArray = top50.map((song) => song.artist);
+    const data = [];
+    function artistSet(value, set) {
+      data.push(value);
+    }
+    new Set(artistArray).forEach(artistSet);
+    res.status(200).json({
+      status: 200,
+      data,
+    });
+  })
+
+  //   var names = ["Mike","Matt","Nancy","Adam","Jenny","Nancy","Carl"];
+  // var uniqueNames = [];
+  // $.each(names, function(i, el){
+  //     if($.inArray(el, uniqueNames) === -1) uniqueNames.push(el);
+  // });
   // add new endpoints here ☝️
   // ---------------------------------
   // Nothing to modify below this line
